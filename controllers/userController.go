99eed10978877de201162477583abd3c0cfbe170
EDR3X <dhunganaannanta@gmail.com>
2022-08-26 22:06:24 +0545

    Login

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,8 +4,11 @@ import (
 	"edr3x/go-jwt/initializers"
 	"edr3x/go-jwt/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -55,5 +58,63 @@ func SignUp(c *gin.Context){
     c.JSON(http.StatusOK, gin.H{
         "message": "User Created Successfully",
     })
+}
+
+// Login 
+func Login(c *gin.Context){
+    // Get email and password from request body
+    var body struct{
+        Email string
+        Password string
+    } 
+    if c.Bind(&body) != nil{
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": "Failed to read body",
+        })        
+        return
+    }
+
+    // Look up requested user in database
+    var user models.User
+    initializers.DB.First(&user, "email = ?", body.Email)
+
+    if user.ID == 0{
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": "Invalid Email or password",
+        })
+        return
+    }
+
+    // Compare password with password saved in database
+    err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(body.Password))
+
+    if err != nil{
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": "Invalid Password",
+        })
+    }
+
+    // Generate jwt token
+    userToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "sub": user.ID,
+        "exp": time.Now().Add(time.Hour * 24 *30).Unix(),
+    })
+    
+    tokenString, err := userToken.SignedString([]byte(os.Getenv("SECRET")))
+
+    if err != nil{
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": "Failed to create token",
+        })
+        return
+    }
+
+    // Set Cookie
+    c.SetSameSite(http.SameSiteLaxMode)
+    c.SetCookie("Authorization", tokenString, 3600 * 24 *30, "", "", false, true)
+              //name           , value      , maxAge    ,path, domain,secure, httpOnly
+
+    // Send jwt token back to user
+    c.IndentedJSON(http.StatusOK,gin.H{})
 }
 
--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func main() {
 
     r.POST("/signup", controllers.SignUp)
 
+    r.POST("/login", controllers.Login)
+
     r.Run()
 
 }
